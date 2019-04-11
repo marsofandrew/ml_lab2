4984e608ee0b14d946a6024d1a8275ca9625c3f9
Andrew Petrov <andrew.petrov@emc.com>
2019-04-11 22:25:03 +0300

    add generation data for graphviz

--- lab3/part1.py
+++ lab3/part1.py
@@ -8,6 +8,12 @@ CRITERION = ['gini', 'entropy']
 MAX_FEATURES = ['auto', 'sqrt', 'log2', None]
 SPLIT_TYPES = ['best', 'random']
 PARTS = 10
+BEST_DEFAULT = 0
+
+
+def step0():
+    quantity = common.learn_and_count_quantity(DecisionTreeClassifier(), data, PARTS)
+    print(quantity)
 
 
 def step1():
@@ -15,7 +21,9 @@ def step1():
     for i in range(1, MAX_DEPTH + 1):
         results[i] = common.learn_and_count_quantity(DecisionTreeClassifier(max_depth=i), data, PARTS)
     common.show_plot_from_dict(results, "quantity(max_depth)", "correct predictions", "max_depth")
-    print(common.find_key_of_max_value(results))
+    best_key = common.find_key_of_max_value(results)
+    print(best_key)
+    BEST_DEFAULT = results[best_key]
 
 
 def step2():
@@ -31,12 +39,14 @@ def step2():
 
     print("max_depth", "max_features", "splitter", "criterion", "quantity")
     for element in results2.keys():
-        print(element, results2.get(element))
+        if results2.get(element) >= BEST_DEFAULT * 0.95:
+            print(element, results2.get(element))
     print("best", common.find_key_of_max_value(results2))
 
 
 if __name__ == '__main__':
     raw_data = np.loadtxt("../glass.csv", delimiter=",", dtype=np.str)
     data = np.array(raw_data[1:, 2:], dtype=np.float)
+    step0()
     step1()
     step2()
--- lab3/part2.py
+++ lab3/part2.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python
+import os
+os.environ["PATH"] += 'C:\Program Files (x86)\Graphviz2.38\bin\dot.exe'
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
 from common_utilities import common
+import graphviz
+
 
 PARTS = 4
 
 if __name__ == '__main__':
     raw_data = np.loadtxt("../Lenses.txt", delimiter=",", dtype=np.int)
     data = np.array(raw_data[:, 1:], dtype=np.int)
-
-    # TODO: show classifier
     classifier = DecisionTreeClassifier()
     quantity = common.learn_and_count_quantity(classifier, data, PARTS)
+    dot_data = tree.export_graphviz(classifier)
+    graph = graphviz.Source(dot_data)
+    graph.save("graph", "..")
+    #graph.render()
     print(quantity)
     print(classifier.predict([[2, 1, 2, 1]]))
