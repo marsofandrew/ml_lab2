ba2fd82cc5df630f6e3a151be5638857ad0cd5fb
Petrov Andrew <andrew.petrov@emc.com>
2019-04-19 22:45:06 +0300

    add parameter features_names in helper function at 3rd lab

--- lab3/helper.py
+++ lab3/helper.py
@@ -3,8 +3,8 @@ import pydotplus
 from sklearn import tree
 
 
-def create_graph_png(classifier, file_path):
-    dot_data = tree.export_graphviz(classifier)
+def create_graph_png(classifier, file_path, features_names=None):
+    dot_data = tree.export_graphviz(classifier, features_names)
     graph = pydotplus.graph_from_dot_data(dot_data)
     Image(graph.create_png())
     graph.write_png(file_path)
